c50225f48e3ff443163f1bcf9c4d831baf12a901
andylamax <andylamax@programmer.net>
2024-01-25 02:48:01 +0300

    Pi tests are all passing now

--- api/smes/src/commonMain/kotlin/flame/SmesApiFlix.kt
+++ api/smes/src/commonMain/kotlin/flame/SmesApiFlix.kt
@@ -5,6 +5,7 @@ import io.ktor.client.request.get
 import kase.response.getOrThrow
 import keep.load
 import kollections.List
+import kollections.ListSerializer
 import koncurrent.Later
 import koncurrent.later
 import koncurrent.later.await
@@ -16,8 +17,9 @@ class SmesApiFlix(private val config: SmeApiFlixOptions) : SmesApi {
     private val logger by config.logger
     override fun load(options: LoadOptions): Later<List<SmeDto>> = config.scope.later {
         val trace = logger.trace(config.message.smes())
+        val secret = config.cache.load(config.sessionCacheKey, UserSession.serializer()).await().secret
         config.http.get(config.routes.smes()) {
-            bearerAuth(config.cache.load<UserSession>(config.sessionCacheKey).await().secret)
-        }.getOrThrow(config.codec, trace)
+            bearerAuth(secret)
+        }.getOrThrow(ListSerializer(SmeDto.serializer()), config.codec, trace)
     }
 }
