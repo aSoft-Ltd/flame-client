a8a47c1dbd605e2fe4ab5427a11193b8814f0ac9
andylamax <andylamax@programmer.net>
2024-02-01 08:36:26 +0300

    Release Opportunities for pimonitor/capital

--- scenes/tests/smes/src/commonMain/kotlin/flame/routes/collection/SmesSceneTest.kt
+++ scenes/tests/smes/src/commonMain/kotlin/flame/routes/collection/SmesSceneTest.kt
@@ -1,15 +1,12 @@
 package flame.routes.collection
 
-import kase.Success
 import kommander.expect
-import kommander.toBe
 import koncurrent.later.await
-import kotlinx.coroutines.test.runTest
-import kotlin.test.Test
 
 abstract class SmesSceneTest(private val scene: SmesScene) {
     protected suspend fun initializationTest() {
         scene.initialize().await()
-        expect(scene.ui.value).toBe<Success<*>>()
+        val page = scene.paginator.loadFirstPage().await()
+        expect(page.number).toBe(1)
     }
 }
