88d0ebc261e158baed7f0b21006a809d6fe3f022
andylamax <andylamax@programmer.net>
2024-02-14 13:47:43 +0300

    Fixed funding list problems

--- api/smes/src/commonMain/kotlin/flame/SmeMonitorApiFlix.kt
+++ api/smes/src/commonMain/kotlin/flame/SmeMonitorApiFlix.kt
@@ -21,4 +21,12 @@ class SmeMonitorApiFlix(private val config: SmeApiFlixOptions) : SmeMonitorApi {
             bearerAuth(secret)
         }.getOrThrow(ListSerializer(SmeDto.serializer()), config.codec, trace)
     }
+
+    override fun load(uid: String): Later<SmeDto> = config.scope.later{
+        val trace = logger.trace(config.message.load())
+        val secret = config.cache.load(config.sessionCacheKey, UserSession.serializer()).await().secret
+        config.http.get(config.routes.load(uid)) {
+            bearerAuth(secret)
+        }.getOrThrow(SmeDto.serializer(), config.codec, trace)
+    }
 }
